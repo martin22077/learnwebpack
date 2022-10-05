aa01a9e92f7ea244a39c5461eb0a380516aa3f4e
MartinRoomet Turja <MM-21-358@tptlive.ee>
2022-10-05 13:57:47 +0300

    added rick and morty page

--- src/index.js
+++ src/index.js
@@ -5,22 +5,27 @@ import { createApp } from 'vue';
 import * as VueRouter from 'vue-router';
 
 import App from './App.vue';
-
 import ToDoList from './pages/ToDoList.vue';
-
 import Modals from './pages/Modals.vue';
+import Clicker from './pages/Clicker.vue';
+import RickAndMorty from './pages/RickAndMorty.vue';
+
 
 const routes = [
-    {path: '/', component: Modals, name: 'home'},
-    {path: '/todolist', component: ToDoList, name: 'ToDoList'},
+    { path: '/rickandmorty', component: RickAndMorty, name: 'RickAndMorty'},
+    { path: '/clicker', component: Clicker, name: 'Clicker' },
+    { path: '/modal', component: Modals, name: 'Modal' },
+    { path: '/todolist', component: ToDoList, name: 'To-Do List' },
 ]
+
 const router = VueRouter.createRouter({
+    // 4. Provide the history implementation to use. We are using the hash history for simplicity here.
     history: VueRouter.createWebHashHistory(),
-    routes, 
-  })
+    routes, // short for `routes: routes`
+})
 
 
-let app =createApp(App);
+let app = createApp(App);
 app.use(router);
 
 app.mount('#app');
